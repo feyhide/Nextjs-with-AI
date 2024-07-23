824c3940669f8387ad9fc4aa24ad26d9896cb2a2
MUHAMMAD FAHAD <141734135+fahad-0900@users.noreply.github.com>
2024-07-24 01:30:18 +0500

    creating a page signin use see frontend, also copying code from nextauth for signin, context,wrapping in layout with authprovider

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,8 +12,9 @@ export async function middleware(request: NextRequest) {
          url.pathname.startsWith("/"))){
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
-    //return NextResponse.redirect(new URL('/home', request.url))
-    
+    if(!token && url.pathname.startsWith('/dashboard')){
+        return NextResponse.redirect(new URL('/sign-in', request.url))
+    }
 }
  
 export const config = {
