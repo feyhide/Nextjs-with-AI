fcd39f5cda8c812c1558b63eaf2a0d10655fdd10
MUHAMMAD FAHAD <141734135+fahad-0900@users.noreply.github.com>
2024-07-24 03:15:00 +0500

    im dumb

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -12,7 +12,7 @@ async function dbConnect(): Promise<void> {
         return
     }
     try {
-        const db = await mongoose.connect(process.env.MONGODB_URL||'',{})
+        const db = await mongoose.connect(process.env.MONGODB_URI||'',{})
         connection.isConnected = db.connections[0].readyState
         
         console.log("db connected successfully")
